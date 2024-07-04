1fec7b735af61ee6484b8fabd25b2bf9f1783576
Daniel <daniel@nuc3>
2024-07-04 13:17:52 +1000

    Added report generation.

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import decimal
+import json
 
 import pandas as pd
 
@@ -132,18 +133,123 @@ def _populate_objects(filename):
                            capacity=row["capacity"])
         else:
             raise Exception(f"Unknown object type encountered during setup:\n {row}")
-    
-def create_report(vpp_name, year_month):
-    # The VPP gets its daily rate first.
 
-    # The VPP gets its first slice according to its own percentage value.
+# Lookup VPP by identifier.
+def _2get_vpp_by_name(name):
+    for vpp in VPPS:
+        if vpp.name == name:
+            return vpp
+    raise Exception(f"VPP with name {name} could not be found.")
+
+def _2get_site_by_nmi(nmi):
+    for site in SITES:
+        if site.nmi == nmi:
+            return site
+    raise Exception(f"Site with nmi {nmi} could not be found.")
+
+def _get_events_df():
+    """
+    Compiles the df on the fly. Can be replaced by a different,
+    more efficient implementation later.
+    """
+    df = pd.read_csv("test_events.csv")
+    # Keep all nmis lower case
+    df["nmi"] = df["nmi"].str.lower()
+    return df
+
+def _populate_vpp_to_events(df):
+    # Populate the vpp information for later analysis.
+    def add_site(row):
+        site = _2get_site_by_nmi(row["nmi"])
+        row["vpp"] = site.vpp_name
+        return row
+    df = df.apply(add_site, axis=1)
+    return df
+
+def _get_battery_df():
+    # Convert to dataframe
+    batteries = []
+    for battery in BATTERIES:
+        new_battery = {
+            "nmi": battery.nmi,
+            "capacity": battery.capacity,
+            "vpp": _2get_site_by_nmi(battery.nmi).vpp_name
+        }
+        batteries.append(new_battery)
+
+    df = pd.DataFrame(batteries)
+    return df
+        
+def create_report(vpp_name, year_month):
+    """
+    In practice each block would probably be a separate function
+    that could be individually tested. Important as it's the main
+    rule engine to allocate funds.
+    
+    Assume that year_month is of the format "YYYY-MM".
+    """
+    vpp = _2get_vpp_by_name(vpp_name)
+    
+    # Data generated from the df of events.
+    df = _get_events_df()
+    df = _populate_vpp_to_events(df)
+
+    # Filter for the vpp and month requested.
+    date_filter = df["date"].str.startswith(year_month)
+    name_filter = df["vpp"] == vpp_name
+    df = df[date_filter & name_filter]
+
+    # VPP daily fees
+    vpp_daily_fee = vpp.daily_fee * 28 # Assumed 28 day month.
+
+    # VPP proportion of fees
+    df["fees"] = df["energy"] * df["tarif"]
+    df["fee_to_vpp"] = df["fees"] * vpp.revenue_percentage
 
     # Each site gets 80% directly.
+    df["fee_direct_to_site"] = (df["fees"] - df["fee_to_vpp"]) * .8
+    site_direct_fees = dict(df.groupby("nmi")["fee_direct_to_site"].sum())
+        
+    # Apportion remaining 20% to each site in proportion to their batteries.
+    # Total to share
+    df["fee_to_share"] = df["fees"] - df["fee_to_vpp"] - df["fee_direct_to_site"]
+    total_fee_to_share = df["fee_to_share"].sum()
+    # Assigning proportions from battery capacity.
+    df_bat = _get_battery_df()
+    df_bat = df_bat[df_bat["vpp"] == vpp_name]
+    total_capacity = df_bat["capacity"].sum()
+    df_nmi_proportion = df_bat.groupby("nmi")["capacity"].sum() / total_capacity
+    site_proportional_fees = dict(df_nmi_proportion * total_fee_to_share)
 
-    # Each site gets the remaining amount by proportion of its battery.
-    pass
+    # Collect site results.
+    site_results = []
+    sites_for_vpp = set(df_bat["nmi"]) # Assumes each site has at least 1 battery.
+    for site in sites_for_vpp:
+        result = {
+            "nmi": site
+        }
+        # Direct fees. 0 if no fees were collected.
+        if site in site_direct_fees:
+            result["direct_fees"] = f"{round(site_direct_fees[site], 4)}"
+        else:
+            result["direct_fees"] = "0"
+        # The 20% shared fees
+        if site in site_proportional_fees:
+            result["shared_fees"] = f"{round(site_proportional_fees[site], 4)}"
+        else:
+            result["shared_fees"] = "0"
+
+        site_results.append(result)
+            
+    # Compile the results into a report.
+    results = {
+        "name": vpp_name,
+        "daily_fee_for_month": f"{round(vpp_daily_fee, 4)}",
+        "sites": site_results
+    }
+    return results
 
 if __name__ == "__main__":
     _populate_objects("test_objects.csv")
-    for obj in VPPS + SITES + BATTERIES:
-        print(obj)
+    report_results = create_report("Bellossom", "2022-10")
+    print(json.dumps(report_results, indent=4))
